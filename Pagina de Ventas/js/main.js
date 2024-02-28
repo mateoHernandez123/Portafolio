eb31ea76136302e32cf8c3a4516e742b65ce1178
mateoHernandez123 <mateo.e.2003hernandez@gmail.com>
2024-02-28 19:22:28 -0300

    Arreglos y listo para publicar

--- Pagina de Ventas/js/main.js
+++ Pagina de Ventas/js/main.js
@@ -92,7 +92,7 @@ function agregarAlCarrito(e) {
         position: "right", // `left`, `center` or `right`
         stopOnFocus: true, // Prevents dismissing of toast on hover
         style: {
-          background: "linear-gradient(to right, #4b33a8, #785ce9)",
+          background: "linear-gradient(to right, #ce5007, #eb7835)",
           borderRadius: "2rem",
           textTransform: "uppercase",
           fontSize: ".75rem"
